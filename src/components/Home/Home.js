ceea9b7128f5539a4b5c4c45225a33bca4fa0a5c
Raghavendra Koppula <rpranith@live.unc.edu>
2021-11-13 00:18:07 -0500

    some setup for firebase and example to get data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { db } from '../../firebase/firebase';
+
 
 const App = () => {
+
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const data = await getDocs(collection(db, "users"));
+            data.forEach((doc) => console.log(doc.data()))
+        };
+
+        getUsers()
+    });
+
     return (
-        <div>Home</div>
+        <div>
+            <div>Home</div>
+            {console.log(user)}
+      </div>
     );
 }
 
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {initializeApp} from 'firebase/app'
+import {getFirestore } from '@firebase/firestore'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+let localId = process.env.FIREBASE_PROJECT_ID;
+console.log(localId);
+
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -16,6 +14,7 @@ const firebaseConfig = {
   measurementId: process.env.MEASUREMENT_ID
 };
 
-// Initialize Firebase
+console.log(firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
