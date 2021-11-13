7d4ba346de15e3624b3e276757086bcb33a80dcc
Kendall Howard <kenxh@email.unc.edu>
2021-11-13 16:05:33 -0500

    Login and Signup components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import React from 'react';
 import { Route, Switch } from 'react-router';
+import Login from './components/Login/Login';
 
 import HomeComponent from './components/Home/Home'
+import Signup from './components/Login/Signup';
 
 function App() {
   return (
     <main>
       <Switch>
         <Route path="/home" component={HomeComponent} exact />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
       </Switch>
     </main>
   );
-}
+} 
 
 export default App;
--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -7,15 +7,18 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: "AIzaSyCn1GgD2YURrZ7PHPedjWyOgMvwNdhac80",
+  authDomain: "porch-232fa.firebaseapp.com",
+  projectId: "porch-232fa",
+  storageBucket: "porch-232fa.appspot.com",
+  messagingSenderId: "716863162709",
+  appId: "1:716863162709:web:115af54068b4413ea007dc",
+  measurementId: "G-G428C8R4BR"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
+
+
+export default app;
