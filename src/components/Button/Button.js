95e62fee0f0ea268773f7be8d25283a4961f0a71
Alexander Yoshida <ayoshida@unc.edu>
2022-03-01 12:40:52 -0500

    added href and image id to button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../index.css';
+import '../../index.css';
+import './Button.css';
 
-export default function Button({id, text, icon, func}) {
-    return <button
-        id={id}
-        onClick={func}
-    >{text}<img width="32" src={icon}/></button>
+export default function Button({id, text, icon=null, func=null, href=null}) {
+    return <a href={href}>
+        <button
+            id={id}
+            onClick={func}
+        >{text}<img id={id + "-img"} width="32" src={icon}/></button>
+    </a>
 
     //for other parts, make folder with your item + css folder to import
 }
