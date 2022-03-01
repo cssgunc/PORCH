e73608bbd11caa135385cb2dfb22ff64f304e6e9
Alexander Yoshida <ayoshida@unc.edu>
2022-03-01 00:12:02 -0500

    partially implemented donate button

--- src/components/DonorDashboard/DonorDashboard.js
+++ src/components/DonorDashboard/DonorDashboard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import TopNavBar from "../TopNavBar/TopNavBar";
 import Donation from "./Donation";
+import Button from "../Button";
+import donate from "./donate.svg";
 
 const App = () => {
     return (
@@ -8,7 +10,12 @@ const App = () => {
             <TopNavBar />
             <p id="donorTitle">Dashboard</p>
             <div id="donationContainer"> <Donation /> <Donation /> </div>
-            <button id="newDonate"> temp Donate Button</button>
+            <div>
+                <Button id="newDonate"
+                        text="Donate"
+                        icon={donate}
+                        func={function() {window.open("https://www.youtube.com/channel/UChAnqc_AY5_I3Px5dig3X1Q")}}/>
+            </div>
         </div>
     );
 }
