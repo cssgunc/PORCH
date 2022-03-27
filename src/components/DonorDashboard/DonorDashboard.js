86cd3059dd43befd015e58ea1ad795f046d4d13d
Elijah Bassett <69734856+BassettUNC@users.noreply.github.com>
2022-03-27 16:34:58 -0400

    Removed Extra Button

--- src/components/DonorDashboard/DonorDashboard.js
+++ src/components/DonorDashboard/DonorDashboard.js
@@ -9,14 +9,10 @@ const App = () => {
             <TopNavBar />
             <p id="donorTitle">Dashboard</p>
             <div id="donationContainer"> <Donation /> </div>
-<<<<<<< HEAD
             <button id="newDonate">Donate
             <img id="Donation-Heart" src={heart} alt="heart icon"/>
             </button>
             <p id="nextDonationDay"></p>
-=======
-            <button id="newDonate">temp Donate Button</button>
->>>>>>> 96ec218da18bccddaef250e5d3acb0a389d4dd3a
         </div>
     );
 }
