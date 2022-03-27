92cf556562baf7019720cf5cce668fa2648012d1
Teertha Penumatcha <teerthap@hotmail.com>
2022-03-27 16:21:53 -0400

    Fixed CSS Donor Dashboard Button

--- src/components/DonorDashboard/DonorDashboard.js
+++ src/components/DonorDashboard/DonorDashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TopNavBar from "../TopNavBar/TopNavBar";
 import Donation from "./Donation";
+import heart from "./donation-heart.png"
 
 const App = () => {
     return (
@@ -8,7 +9,10 @@ const App = () => {
             <TopNavBar />
             <p id="donorTitle">Dashboard</p>
             <div id="donationContainer"> <Donation /> </div>
-            <button id="newDonate"> temp Donate Button</button>
+            <button id="newDonate">Donate
+            <img id="Donation-Heart" src={heart} alt="heart icon"/>
+            </button>
+            <p id="nextDonationDay"></p>
         </div>
     );
 }
